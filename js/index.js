ee396a6f0fbb35e60624ffdcbdd96299b859cb6b
Taylor <taylorhahn27@gmail.com>
2019-10-09 18:57:40 -0700

    my git commits are messed up.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+$("#my_form").submit(function(event) {
+  event.preventDefault(); //prevent default action
+  var post_url = $(this).attr("action"); //get form action url
+  var request_method = $(this).attr("method"); //get form GET/POST method
+  var form_data = $(this).serialize(); //Encode form elements for submission
+
+  $.ajax({
+    url: post_url,
+    type: request_method,
+    data: form_data
+  }).done(function(response) {
+    //
+    $("#server-results").html(response);
+  });
+});
+
 //menu replacing content
 // $(".button")
 //   .first()
@@ -54,3 +70,7 @@ function show(id) {
     id
   ).innerHTML;
 }
+
+// Use the CDN or host the script yourself
+// https://cdnjs.cloudflare.com/ajax/libs/instafeed.js/1.4.1/instafeed.min.js
+// https://matthewelsom.com/assets/js/libs/instafeed.min.js
